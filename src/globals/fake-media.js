df8ea03038965f10236076f28d5b721f6a977d65
Sofia Sousa <csofiamsousa@gmail.com>
2019-01-29 12:25:31 +0000

    refactor: Change getMedia to handle with different media types (ex: video)

--- src/globals/fake-media.js
+++ src/globals/fake-media.js
@@ -3,23 +3,31 @@ const date = (new Date()).toISOString();
 // List of images
 export const medias = [];
 
-export function getMedia (id, type, src) {
+export function getMedia (id, params = {}) {
+  const sizes = {};
+
+  if (params.thumbnail) {
+    sizes.thumbnail = {
+      source_url: params.thumbnail,
+    };
+  }
+
   return {
     id,
     title: { raw: '', rendered: '' },
     caption: { raw: '', rendered: '' },
     date_gmt: date,
     date,
-    media_type: 'image',
-    mime_type: type || 'image/jpeg',
-    source_url: src || `${window.location.origin}/img${id}.png`,
+    media_type: params.media_type || 'image',
+    mime_type: params.mime_type || 'image/jpeg',
+    source_url: params.source_url || `${window.location.origin}/img${id}.png`,
     // link: `${window.location.origin}/img${id}.png`,
     media_details: {
       file: '',
       width: 0,
       height: 0,
       image_meta: {},
-      sizes: {},
+      sizes,
     },
   };
 }
@@ -37,7 +45,22 @@ export function createMedia (file) {
   });
 }
 
-// Load media
-for(let i = 0; i < 3; i++) {
+// Load media (images)
+for(let i = 0; i < 4; i++) {
   medias.push(getMedia(i + 1));
 }
+
+// Load media (videos)
+medias.push(getMedia(4, {
+  media_type: 'video',
+  mime_type: 'video/mp4',
+  source_url: `${window.location.origin}/video1.mp4`,
+  thumbnail: 'https://i.vimeocdn.com/video/570251592_640x360.jpg',
+}));
+
+medias.push(getMedia(5, {
+  media_type: 'video',
+  mime_type: 'video/mp4',
+  source_url: `${window.location.origin}/video2.mp4`,
+  thumbnail: 'https://i.vimeocdn.com/video/543433483_640x360.jpg',
+}));
