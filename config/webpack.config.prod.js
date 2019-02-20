7e742d1e4bd91342d3ac6755a4aa00dae166b3be
Elio Cro <elio.cro@gmail.com>
2019-02-20 11:06:27 +0000

    fix: Disable compression to fix circular dependency issue

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -136,20 +136,7 @@ module.exports = {
             // https://github.com/facebook/create-react-app/pull/4234
             ecma: 8,
           },
-          compress: {
-            ecma: 5,
-            warnings: false,
-            // Disabled because of an issue with Uglify breaking seemingly valid code:
-            // https://github.com/facebook/create-react-app/issues/2376
-            // Pending further investigation:
-            // https://github.com/mishoo/UglifyJS2/issues/2011
-            comparisons: false,
-            // Disabled because of an issue with Terser breaking valid code:
-            // https://github.com/facebook/create-react-app/issues/5250
-            // Pending futher investigation:
-            // https://github.com/terser-js/terser/issues/120
-            inline: 2,
-          },
+          compress: false,
           mangle: {
             safari10: true,
           },
