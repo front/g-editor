717371d760e3b8fcb0fc226d60dbf9725e5178e4
comerc <comerc@users.noreply.github.com>
2019-10-12 17:10:22 +0300

    Added handlers for categories and tags

--- src/globals/api-routes.js
+++ src/globals/api-routes.js
@@ -5,6 +5,7 @@ import { mediaList, createMedia } from './fake-media.js';
 import users from '../data/users';
 import taxonomies from '../data/taxonomies';
 import categories from '../data/categories';
+import tags from '../data/tags';
 import types from '../data/types';
 import themes from '../data/themes';
 
@@ -176,12 +177,41 @@ export default [
   // Categories
   {
     path: '/wp/v2/categories',
-    method: '*',
+    method: 'GET',
     handler () {
-      return categories;
+      return new window.Response(JSON.stringify(categories));
+    },
+  },
+  {
+    path: '/wp/v2/categories',
+    method: 'POST',
+    handler({ payload: { name } }) {
+      return {
+        id: window.lodash.random(1, 100000000),
+        name,
+      };
     },
   },
 
+  // Tags
+  {
+    path: '/wp/v2/tags',
+    method: 'GET',
+    handler() {
+      return new window.Response(JSON.stringify(tags));
+    },
+  },
+  {
+    path: '/wp/v2/tags',
+    method: 'POST',
+    handler({ payload: { name } }) {
+      return {
+        id: window.lodash.random(1, 100000000),
+        name,
+      };
+    },
+  },
+  
   // Users
   {
     path: '/wp/v2/users/',
