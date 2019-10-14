bfd203345b6b7fa6a83c8047eb47cf2ef20a3ae5
Sofia Sousa <csofiamsousa@gmail.com>
2019-10-14 09:04:53 +0100

    feat: Change wp packages order

--- src/core/api-fetch.js
+++ src/core/api-fetch.js
@@ -8,7 +8,20 @@ const {
   setFetchHandler,
 } = window.wp.apiFetch;
 
-use(createNonceMiddleware(window.wpApiSettings.nonce));
+const nonceMiddleware = createNonceMiddleware(window.wpApiSettings.nonce);
+
+use(nonceMiddleware);
+
+window.wp.hooks.addAction(
+  'heartbeat.tick',
+  'core/api-fetch/create-nonce-middleware',
+  function (response) {
+    if (response.rest_nonce) {
+      nonceMiddleware.nonce = response.rest_nonce;
+    }
+  }
+);
+
 use(createRootURLMiddleware(window.wpApiSettings.root));
 
 setFetchHandler(apiFetch);
